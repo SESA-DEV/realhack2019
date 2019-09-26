6746272fb09fae746eeace769fd2a0708e05a2e3
BurbaCodes <dilushadasanayaka@gmail.com>
2019-09-26 20:30:42 +0530

    mail settings

--- contact_process.php
+++ contact_process.php
@@ -1,10 +1,8 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "dilushadasanayaka@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
     $cmessage = $_REQUEST['message'];
 
     $headers = "From: $from";
@@ -26,12 +24,11 @@
 	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
 	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
 	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
 	$body .= "<tr><td></td></tr>";
 	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
 	$body .= "</tbody></table>";
 	$body .= "</body></html>";
 
-    $send = mail($to, $subject, $body, $headers);
+    $send = mail($to, 'test', $body, $headers);
 
 ?>
