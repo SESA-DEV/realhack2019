5a39f9eb6262270feb4ec28da13625e578d0e846
dasaCoder <dilushadasanayaka@gmail.com>
2019-10-18 11:11:30 +0530

    test

--- js/custom.js
+++ js/custom.js
@@ -17,7 +17,7 @@
 
     function makeTimer() {
 
-      var endTime = new Date("17 October 2019 23:59:59 GMT+05:30");			
+      var endTime = new Date("28 October 2019 23:59:59 GMT+05:30");			
       endTime = (Date.parse(endTime) / 1000);
   
       var now = new Date();
