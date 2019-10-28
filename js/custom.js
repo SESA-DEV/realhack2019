8b7c6e4472054f4ab4d64d2ccd37263a078d5947
BurbaCodes <dilushadasanayaka@gmail.com>
2019-10-28 21:56:55 +0530

    registration closed

--- js/custom.js
+++ js/custom.js
@@ -17,7 +17,7 @@
 
     function makeTimer() {
 
-      var endTime = new Date("28 October 2019 23:59:59 GMT+05:30");			
+      var endTime = new Date("02 November 2019 06:00:00 GMT+05:30");			
       endTime = (Date.parse(endTime) / 1000);
   
       var now = new Date();
