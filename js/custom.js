5c4f4d5e878409da4b6c5b509942e625de2db1c4
BurbaCodes <dilushadasanayaka@gmail.com>
2019-10-28 22:18:06 +0530

    change prehack time

--- js/custom.js
+++ js/custom.js
@@ -17,7 +17,7 @@
 
     function makeTimer() {
 
-      var endTime = new Date("02 November 2019 06:00:00 GMT+05:30");			
+      var endTime = new Date("02 November 2019 07:00:00 GMT+05:30");			
       endTime = (Date.parse(endTime) / 1000);
   
       var now = new Date();
